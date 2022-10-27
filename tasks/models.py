cd37476f8dfc312fdf1ad2746234dfdfd9feffcf
Cristian David <cristiandaberu@gmail.com>
2022-10-27 14:49:31 -0500

    firstcommit

---
+++ tasks/models.py
@@ -0,0 +1,19 @@
+from email.policy import default
+from io import open_code
+from multiprocessing.spawn import import_main_path
+from turtle import title
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' - by ' + self.user.username
